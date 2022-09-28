f151a3af5615f723560fe289dfc63688e2485041
doikuomola <doikuomola@gmail.com>
2022-09-28 07:53:30 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith('VITE_')) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
 });
+
+export default {
+  alias: {
+    // eslint-disable-next-line global-require
+    '@': require('path').resolve(__dirname, 'src'),
+  },
+  define: viteEnv,
+  plugins: [react()],
+};
